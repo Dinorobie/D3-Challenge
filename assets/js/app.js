2abec2a8cb5548a267979a3f2a5721912e411d3e
Robert Sepulveda <robert@MacBook-Pro.local>
2021-07-10 16:51:13 -0500

    Add bouble graph to js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,133 @@
-// @TODO: YOUR CODE HERE!
+const svgWidth = 1050;
+const svgHeight = 500;
+
+const margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+const svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight + 40); // extra padding for third label
+
+// Append an SVG group
+const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+
+
+    // Import Data
+  
+    // Parse Data/Cast as numbers
+    d3.csv("assets/data/data.csv").then(function(data) {
+      console.log(data)
+      data.forEach(function(data){
+        data.poverty    = +data.poverty;
+        data.healthcare = +data.healthcare;
+        data.age        = +data.age;
+        data.smokes     = +data.smokes;
+        data.obesity    = +data.obesity;
+        data.income     = +data.income;
+      });
+    //Creat Scale function
+      var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d.poverty) * 0.8,
+      d3.max(data, d => d.poverty) * 1.2
+    ])
+    .range([0, width]);  
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.healthcare)])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+   chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis); 
+
+  // append y axis
+  chartGroup.append("g")
+  .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 12)
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
+
+    // Create group for two x-axis labels
+  var labelsGroup = chartGroup.append("g")
+  .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    chartGroup.selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function(d){
+      console.log (d.abbr)
+      return `${d.abbr}`;
+    })
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .attr("fill", "black")
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "central");
+    
+    //Initialys Tool Tip
+    var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.state}<br>Poverty; ${d.poverty}<br>healthcare; ${d.healthcare}`);
+    });
+
+    circlesGroup.call(toolTip);
+    
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+  // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left+40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("People with out health care");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .style("font", "20px sans-serif")
+    .style("font-weight", "bold")
+    .text("Poverty (%)");
+ 
+
+
+    });
